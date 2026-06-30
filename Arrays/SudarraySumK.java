46b076c4c1b625f3e60bb18e9ee3bc96795077d2
Mekala Sandhya <sandhyamekala435@gmail.com>
2026-06-30 11:03:49 +0530

    Prefix sum technique

---
+++ Arrays/SudarraySumK.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int cntSubarrays(int[] arr, int k) {
+        // code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int prefixsum=0;
+        int count=0;
+        map.put(0,1);
+        for(int i=0;i<arr.length;i++){
+            prefixsum+=arr[i];
+            if(map.containsKey(prefixsum-k)){
+                count+=map.get(prefixsum-k);
+            }
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
+        }
+        return count;
+    }
+}
