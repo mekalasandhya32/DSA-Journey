8d26f1dac4c96f16336ed4f36def7659d9745c48
Mekala Sandhya <sandhyamekala435@gmail.com>
2026-07-01 10:44:39 +0530

    Prefix sum Technique

---
+++ Arrays/ProductArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int[] productExceptSelf(int arr[]) {
+        // code here
+        int[] res=new int[arr.length];
+        int[] prefix=new int[arr.length];
+        int[] suffix=new int[arr.length];
+        prefix[0]=1;
+        for(int i=1;i<arr.length;i++){
+            prefix[i]=arr[i-1]*prefix[i-1];
+        }
+        suffix[arr.length-1]=1;
+        for(int i=arr.length-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*arr[i+1];
+        }
+        for(int i=0;i<arr.length;i++){
+            res[i]=prefix[i]*suffix[i];
+        }
+        return res;
+    }
+}
